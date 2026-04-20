a804683de30a815932c13bab22e335b5921f3c18
SHUBHRANSHU SHEKHAR BHOI <64605129+akamabhoi@users.noreply.github.com>
2026-04-20 22:35:03 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SocketProvider } from './context/SocketContext'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
